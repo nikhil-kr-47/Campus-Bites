254e27a7cf491e71d3ac4f848f2108a500e5cba0
Nikhil Kumar <nikhilkumarkushwaha.cse2023@ritroorkee.com>
2026-05-29 22:06:52 +0530

    Fixed nodemailer ip address issue

--- controllers/user.js
+++ controllers/user.js
@@ -5,6 +5,7 @@ const nodemailer=require("nodemailer");
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
+    family:4,
     secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
     auth: {
       user: process.env.SMTP_USER,
--- index.js
+++ index.js
@@ -65,7 +65,7 @@ main().then(()=>{console.log("Connected to DB")}).catch(err=>console.log(err));
 
 const port=8080;
 
-app.listen(process.env.PORT || port,()=>{
+app.listen(port,()=>{
     console.log("App is listening");
 });
 

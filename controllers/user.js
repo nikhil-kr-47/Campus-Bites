ea3f9b231d10a4407584dffe6c841f5eb4bff5cf
Nikhil Kumar <nikhilkumarkushwaha.cse2023@ritroorkee.com>
2026-05-29 22:27:19 +0530

    Trying to fix nodemailer issue

--- controllers/user.js
+++ controllers/user.js
@@ -3,10 +3,7 @@ const crypto=require("crypto");
 const nodemailer=require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    family:4,
-    secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
+   service:"gmail",
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
